48993ad037f68b894fcd23d413b609be42e4256c
Jade Abreu <110346143+serengetijade@users.noreply.github.com>
2022-09-07 19:45:17 -0800

    LightBox

---
+++ projects/OnePageWebsite/js/OnePageWebsite.js
@@ -0,0 +1,34 @@
+//LIGHTBOX
+function openModal() {  // Open the Modal
+    document.getElementById("myModal").style.display = "block";
+    }
+function closeModal() { // Close the Modal
+    document.getElementById("myModal").style.display = "none";
+}
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {    // Next/previous controls
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {  // Thumbnail image controls
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("lightboxthumb");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+}
+//END LIGHTBOX
